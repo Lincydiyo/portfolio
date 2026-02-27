f7dda70da0fedfa8aa19302fcf4ff70e7fb59ae2
Lincydiyo <lincydiyo39@gmail.com>
2026-02-27 10:49:21 +0530

    Update project links to live Vercel URLs

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -10,84 +10,83 @@ import Dress from "../ImageFolder/Dress.webp";
 
 function Project() {
   return (
-    <>
-      <div className="ProjectDiv">
-        <Container>
-          <Row>
-        
-                <Col xs={12} md={6} lg={3}>
-              <Card className="MainCard">
-                <Card.Img variant="top" src={restaurent} />
-                <Card.Body className="CardBody">
-                  <Card.Title>Restaurent Website</Card.Title>
-                  <button type="button">
-                    <a
-                      href="https://lincydiyo.github.io/Restaurent-Website/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Click Here
-                    </a>
-                  </button>
-                </Card.Body>
-              </Card>
-            </Col>
-                <Col xs={12} md={6} lg={3}>
-              <Card className="MainCard">
-                <Card.Img variant="top" src={library} />
-                <Card.Body className="CardBody">
-                  <Card.Title>Library Management System</Card.Title>
-                  <button type="button">
-                    <a
-                      href="  https://lincydiyo.github.io/Library-Management-System/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Click Here
-                    </a>
-                  </button>
-                </Card.Body>
-              </Card>
-            </Col>
-             <Col xs={12} md={6} lg={3}>
-              <Card className="MainCard">
-                <Card.Img variant="top" src={Dress}/>
-                <Card.Body className="CardBody">
-                  <Card.Title>Dress Website</Card.Title>
-                  <button type="button">
-                    <a
-                      href="  https://lincydiyo.github.io/Dress-Website/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Click Here
-                    </a>
-                  </button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col xs={12} md={6} lg={3}>
-              <Card className="MainCard">
-                <Card.Img variant="top" src={portfolio} />
-                <Card.Body className="CardBody">
-                  <Card.Title>Portfolio</Card.Title>
-                  <button type="button">
-                    <a
-                      href="https://lincydiyo.github.io/portfolio/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Click Here
-                    </a>
-                  </button>
-                </Card.Body>
-              </Card>
-            </Col>
-           
-          </Row>
-        </Container>
-      </div>
-    </>
+    <div className="ProjectDiv">
+      <Container>
+        <Row>
+          <Col xs={12} md={6} lg={3}>
+            <Card className="MainCard">
+              <Card.Img variant="top" src={restaurent} />
+              <Card.Body className="CardBody">
+                <Card.Title>Restaurent Website</Card.Title>
+                <button type="button">
+                  <a
+                    href="https://restaurent-website-sc4x.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Click Here
+                  </a>
+                </button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} md={6} lg={3}>
+            <Card className="MainCard">
+              <Card.Img variant="top" src={library} />
+              <Card.Body className="CardBody">
+                <Card.Title>Library Management System</Card.Title>
+                <button type="button">
+                  <a
+                    href="https://library-management-system-tau-ten.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Click Here
+                  </a>
+                </button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} md={6} lg={3}>
+            <Card className="MainCard">
+              <Card.Img variant="top" src={Dress} />
+              <Card.Body className="CardBody">
+                <Card.Title>Dress Website</Card.Title>
+                <button type="button">
+                  <a
+                    href="https://dress-website-black.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Click Here
+                  </a>
+                </button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} md={6} lg={3}>
+            <Card className="MainCard">
+              <Card.Img variant="top" src={portfolio} />
+              <Card.Body className="CardBody">
+                <Card.Title>Portfolio</Card.Title>
+                <button type="button">
+                  <a
+                    href="https://portfolio-one-gilt-61.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Click Here
+                  </a>
+                </button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
